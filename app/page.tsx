1bce67bdaabc695ea224d5275d361396e952975d
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-11-23 06:24:30 +0000

    typewritereffect

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,31 @@
+'use client'
 import Image from 'next/image'
 
+import React, { useState, useEffect } from "react";
+
 export default function Home() {
+  const word = "Welcome";
+  const [displayedText, setDisplayedText] = useState("");
+  let i = 0;
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (i < word.length) {
+        setDisplayedText((prevState) => prevState + word.charAt(i));
+        i++;
+      } else {
+        clearInterval(timer);
+      }
+    }, 700);
+
+    return () => clearInterval(timer);
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Welcome
+          {displayedText}
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
@@ -57,7 +77,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Ecommerce clothing brand
+            Ecommerce clothing brand(SEO Optimized)
           </p>
         </a>
 
