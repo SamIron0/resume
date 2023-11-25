bcc5726764d72a1822e399e02bfbdd8be8be5760
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-11-25 02:54:24 +0000

    homepageRedo

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import Image from 'next/image'
-
+import HomepageIntroduction from '../components/HomepageIntroduction'
 import React, { useState, useEffect } from "react";
 
 export default function Home() {
@@ -39,11 +39,8 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <h1
-          className='text-3xl'>
-          PORTFOLIO
-        </h1>
+      <div className="h-500px">
+        <HomepageIntroduction/>
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
@@ -60,7 +57,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Meal plan generator using the Open AI API
+            Meal plan generator using the Open AI API. Built usign NextJS, NodeJS, Typescript, React, HTML, CSS
           </p>
         </a>
 
@@ -78,6 +75,40 @@ export default function Home() {
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Ecommerce clothing brand(SEO Optimized)
+          </p>
+        </a>
+
+        <a
+          href="https://vestaatelier.com"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Atelier{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Ecommerce clothing brand(SEO Optimized) website built on Shopify
+          </p>
+        </a>
+
+        <a
+          href="https://vestaatelier.com"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Restaurant Website{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            A resetaurant case study built on Wix platform
           </p>
         </a>
       </div>
